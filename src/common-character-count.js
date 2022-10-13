1a8c97cb3586e468d0cf409afd89c86caa8c2ea8
Ardoreast <lenovolenovs820@gmail.com>
2022-10-13 21:02:00 +0300

    feat: add solution of sortbyheight

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,11 +12,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-let s=(s1+s2).split('')
 let res=[]
-for(char of s) {
-if(!res.includes(char)) res.push(char)
-}
+s1=s1.split('').sort()
+s2=s2.split('').sort()
+if(s2>=s1) {
+for(let i=0; i<s2.length; i++) {
+if(s1[i]===s2[i]) res.push(s2[i])
+}} else if(s1>=s2) {
+for(let i=0; i<s1.length; i++) {
+if(s1[i]===s2[i]) res.push(s1[i])
+}};
 return res.length
 }
 
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -12,7 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
 function sortByHeight(arr) {
-  throw new NotImplementedError('Not implemented');
+  let arrSort = arr.filter(e => e !== -1).sort((a, b) => a - b);
+let arrNew = [];
+let j = 0;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === -1) {
+    arrNew.push(arr[i]);
+  } else {
+    arrNew.push(arrSort[j]);
+    j++;
+  }
+}
+return arrNew
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
